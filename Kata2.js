d0cd8f0a29814875e00d28e31b2d782747ba6bc3
AlexandrBiryukov997 <alexbystryi10@gmail.com>
2022-08-28 00:13:24 +0300

    first task

---
+++ Kata2.js
@@ -0,0 +1,15 @@
+// Напишите функцию, которая вычисляет среднее значение чисел в заданном списке.
+// Примечание. Пустые массивы должны возвращать 0.
+
+// Решение:
+
+function find_average(array) {
+    let numbers = 0;
+  for(i = 0;i < array.length;i++) {
+    if(array.length !== 0) {
+      numbers += array[i];
+    } else numbers = 0;
+  }
+    return numbers / array.length;
+  }
+
